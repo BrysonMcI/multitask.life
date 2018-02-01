619f7ad41e00bc387a20fc7f65e594dbe1016ad2
Ohan <ohan@mail.rit.edu>
2018-02-01 00:39:32 -0500

    basic mongo connection, add and query entries. included insomnia export since that was actually hard to craft properly

--- schema.js
+++ schema.js
@@ -1,18 +1,31 @@
 var graphql = require('graphql');
+var mongoose = require('mongoose');
+/* start mongo stuff */
+const mongoStr = "mongodb://127.0.0.1/mtl";
 
-var todos = [
-    {
-        "due": "Monday",
-        "title": "Discussion post",
-        "completed": false
-    },
-    {
-        "due": "Sunday",
-        "title": "watch superbowl",
-        "completed": false
-    }
-];
+// setup connection
+mongoose.connect(mongoStr, (error) => {
+    if (error) console.error(error)
+    else console.log('mongo connection successful');
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
+// define schema within mongo
+var todoMongo = new mongoose.Schema({
+    due: Date,
+    title: String,
+    completed: Boolean
+});
+
+// register with a model
+var todos = mongoose.model('todo', todoMongo);
+/* end mongo stuff */
+
+/* start gql stuff */
+
+// what does a todo look like to gql?
 var todoType = new graphql.GraphQLObjectType({
     name: 'todo',
     fields: function() {
@@ -21,7 +34,8 @@ var todoType = new graphql.GraphQLObjectType({
                 type: graphql.GraphQLString
             },
             title: {
-                type: graphql.GraphQLString
+                type: graphql.GraphQLString,
+                description: 'what needs to be done'
             },
             completed: {
                 type: graphql.GraphQLBoolean
@@ -30,6 +44,7 @@ var todoType = new graphql.GraphQLObjectType({
     }
 });
 
+// interface for querying todo items
 var queryType = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: function() {
@@ -37,14 +52,82 @@ var queryType = new graphql.GraphQLObjectType({
             todos: {
                 type: new graphql.GraphQLList(todoType),
                 resolve: function() {
-                    return todos;
+                    return new Promise((resolve, reject) => {
+                        todos.find((err, data) => {
+                            if (err) reject(err)
+                            else resolve(data)
+                        })
+                    })
                 }
             }
         }
     }
 });
 
-module.exports = new graphql.GraphQLSchema({
-    query: queryType
+// what happens when we want to add a todo item?
+var mutationAdd = {
+    type: todoType,
+    description: 'Add a Todo',
+    args: {
+      title: {
+        name: 'Todo title',
+        type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+      }
+    },
+    resolve: (root, args) => {
+      var newTodo = new todos({
+        title: args.title,
+        completed: false
+      })
+      // newTodo.id = newTodo._id
+      return new Promise((resolve, reject) => {
+        newTodo.save(function (err) {
+          if (err) reject(err)
+          else resolve(newTodo)
+        })
+      })
+    }
+}
+
+/* reference delete */
+/*
+var MutationDestroy = {
+    type: TodoType,
+    description: 'Destroy the todo',
+    args: {
+      id: {
+        name: 'Todo Id',
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve: (root, args) => {
+      return new Promise((resolve, reject) => {
+        TODO.findById(args.id, (err, todo) => {
+          if (err) {
+            reject(err)
+          } else if (!todo) {
+            reject('Todo NOT found')
+          } else {
+            todo.remove((err) => {
+              if (err) reject(err)
+              else resolve(todo)
+            })
+          }
+        })
+      })
+    }
+  }
+*/
+
+// define our interface for mutations on todo objects
+var mutationType = new graphql.GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        add: mutationAdd
+    }
 });
 
+module.exports = new graphql.GraphQLSchema({
+    query: queryType,
+    mutation: mutationType
+});
